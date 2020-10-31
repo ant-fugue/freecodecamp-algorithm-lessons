e0d2b06165377c887dd1791f815760ea1dbcaee8
searchingforapuppy <ebisulove2@gmail.com>
2020-10-31 23:47:23 +0900

    initial commit

---
+++ sum_all_numbers_in_a_range.js
@@ -0,0 +1,5 @@
+/* 
+  We'll pass you an array of two numbers. 
+  Return the sum of those two numbers plus the sum of all the numbers between them.
+  The lowest number will not always come first.
+*/
